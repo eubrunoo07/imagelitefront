6cd893f016c0bfa6ea26750dee220b6748591dcd
Bruno Silva <devbruninn@outlook.com>
2024-03-18 15:43:35 -0300

    iniciando o projeto front end

---
+++ src/app/form/page.tsx
@@ -0,0 +1,5 @@
+export default function FormPage(){
+    return (
+        <h1>Formulário</h1>
+    )
+}
---
+++ src/app/galery/page.tsx
@@ -0,0 +1,5 @@
+export default function GaleryPage(){
+    return (
+        <h1>Galery</h1>
+    )
+}
